2a56f4bd4157de0614c22fc4f47f3e0a74b62cae
shailesh5498 <ss983908@gmail.com>
2026-02-02 18:17:25 +0530

    remove_link2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           NewsMonkey
         </a>
 
